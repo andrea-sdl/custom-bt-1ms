b79f56596c28f9328891aabcc5060e0076c3d2bd
agrassi <andrea.sdl@gmail.com>
2014-06-15 17:13:30 +0200

    corretto bug visualizzazione

--- extension.js
+++ extension.js
@@ -2,13 +2,13 @@ function showHideSidebarOnSmallDevices()
 {
     if (jQuery("#sidebar-btn-show").is(":visible"))
     {
-        jQuery(".sidebar_content").addClass("sidebar-show");
-        jQuery(".sidebar_content").removeClass("sidebar-hide");
+        jQuery(".sidebar_block").addClass("sidebar-show");
+        jQuery(".sidebar_block").removeClass("sidebar-hide");
         jQuery(".sidebar-btn").toggleClass("sidebar-hide");
     }else
     {
-        jQuery(".sidebar_content").addClass("sidebar-hide");
-        jQuery(".sidebar_content").removeClass("sidebar-show");
+        jQuery(".sidebar_block").addClass("sidebar-hide");
+        jQuery(".sidebar_block").removeClass("sidebar-show");
         jQuery(".sidebar-btn").toggleClass("sidebar-hide");
     }
 
